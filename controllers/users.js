a6e33ded7bb3deb095053d4eb1d24357bdae55a6
Aleksey Tishenkov <tishenkov@gmail.com>
2023-08-07 20:47:55 +0300

    feat: add signup controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,12 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const BadRequestError = require("../errors/request-error");
 const NotFoundError = require("../errors/not-found-error");
 const AuthError = require("../errors/authorization-error");
+const { CREATED, CONFLICT } = require("../errors/error-codes");
+
+const SALT_ROUNDS = 10;
 
 const getUserInfo = (req, res, next) => {
   User.findOne({ _id: res.user._id })
@@ -38,7 +43,32 @@ const updateUserInfo = (req, res, next) => {
 };
 
 const signup = (req, res, next) => {
-	
+  const { name, email, password } = req.body;
+
+  bcrypt.hash(password, SALT_ROUNDS, (err, hash) => {
+    User.create({ name, email, password: hash })
+      .then((user) => {
+        const { name, email } = user;
+        res.status(CREATED).send({ name, email });
+      })
+      .catch((error) => {
+        if (error.name === ("ValidationError" || "CastError")) {
+          next(
+            new BadRequestError(`Data validation error (${error.message})`),
+          );
+          return;
+        }
+        if (error.code === 11000) {
+          next(
+            res.status(CONFLICT).send({
+              message: `User with this email already exists (${error.message})`,
+            }),
+          );
+          return;
+        }
+        next(error);
+      });
+  });
 };
 
 const signin = (req, res, next) => {};
